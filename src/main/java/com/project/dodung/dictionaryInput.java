e37036f843cd6d178996e2d4123f79c7495c0941
tsunN <21020461@vnu.edu.vn>
2022-03-30 19:34:49 +0700

    Upload Database

---
+++ src/main/java/com/project/dodung/dictionaryInput.java
@@ -0,0 +1,16 @@
+package com.project.dodung;
+
+import java.util.ArrayList;
+
+public class dictionaryInput {
+    /**
+     * If the given input is found -> Print the String.
+     * Otherwise, print the f****** "Not found!".
+     * @param input The String to be searched.
+     * @return whether the String is found or not.
+     */
+
+    public String[] findWord(String input) {
+    };
+    
+}
