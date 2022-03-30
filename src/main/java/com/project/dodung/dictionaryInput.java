edbe8be3ebfde81917c2a455e100df576340ff56
tsunN <50802365+Tsun0193@users.noreply.github.com>
2022-03-30 21:43:04 +0700

    .

--- src/main/java/com/project/dodung/dictionaryInput.java
+++ src/main/java/com/project/dodung/dictionaryInput.java
@@ -1,16 +1,21 @@
 package com.project.dodung;
 
-import java.util.ArrayList;
-
 public class dictionaryInput {
     /**
      * If the given input is found -> Print the String.
      * Otherwise, print the f****** "Not found!".
+     *
      * @param input The String to be searched.
      * @return whether the String is found or not.
      */
 
-    public String[] findWord(String input) {
-    };
-    
+    public String findWord(String input) {
+        String ans = "";
+        if (!ans.equals("")) {
+            return ans;
+        } else {
+            return "Not found";
+        }
+    }
+
 }
--- src/main/java/com/project/duydang/DuyDangMain.java
+++ src/main/java/com/project/duydang/DuyDangMain.java
@@ -1,12 +1,17 @@
 package com.project.duydang;
 
 import com.project.main.Main;
+import java.util.*;
+import javax.imageio.*;
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+
 
 public class DuyDangMain extends Main {
 
     @Override
     public void run(){
-
     }
 
 }
