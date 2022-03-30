16a221adcd0cddd887fbec33af45726e78317e9a
Quang Minh Tran <minhtran6464.lhp@gmail.com>
2022-03-31 00:23:51 +0700

    Quang Minh add handler

--- src/main/java/com/project/quangminh/QuagMinhMain.java
+++ src/main/java/com/project/quangminh/QuagMinhMain.java
@@ -6,7 +6,7 @@ public class QuagMinhMain extends Main {
 
     @Override
     public void run(){
-        System.out.println("hello!");
+
     }
 
 }
---
+++ src/main/java/com/project/quangminh/Word.java
@@ -0,0 +1,35 @@
+package com.project.quangminh;
+
+public class Word implements Comparable<Word>{
+    private String targetWord;
+    private String meaningWord;
+
+    public Word(){
+
+    }
+    public Word(String targetWord,String meaningWord){
+        this.targetWord = targetWord;
+        this.meaningWord = meaningWord;
+    }
+
+    public String getTargetWord() {
+        return targetWord;
+    }
+
+    public String getMeaningWord() {
+        return meaningWord;
+    }
+
+    public void setMeaningWord(String meaningWord) {
+        this.meaningWord = meaningWord;
+    }
+
+    public void setTargetWord(String targetWord) {
+        this.targetWord = targetWord;
+    }
+
+    @Override
+    public int compareTo(Word o) {
+        return this.getTargetWord().compareTo(o.getTargetWord());
+    }
+}
